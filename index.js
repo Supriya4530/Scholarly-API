6bc0d01e64012dfb72112f9f78e2326f4dc89fa4
supriya3045 <supriyasukale11@gmail.com>
2025-08-20 16:44:52 +0530

    Add index.js for Scholarly API

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
+
 
 // Health check
 app.get("/", (req, res) => {
